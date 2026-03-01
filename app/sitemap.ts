b237e072ba989dff28f5e69cb6c87cf92f0d5aef
Ing-Valdes-GL <doungmolagoungvaldes@gmail.com>
2026-03-01 14:14:49 +0100

    sitemap ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: 'https://alluvihealth.store', lastModified: new Date() },
